7b4633035acec9b3f5867d3a5d7e9df2f8f344d6
jBuly4 <ibuly4@gmail.com>
2021-04-04 22:16:42 +0300

    something wrong with timeouts

--- server.py
+++ server.py
@@ -1,6 +1,5 @@
 import asyncio
 from collections import namedtuple
-import bisect
 
 requests = namedtuple('requests', 'put get all') # all = '*\n'
 request = requests(put = 'put', get = 'get', all = '*\n')
@@ -24,11 +23,14 @@ def run_server(host, port):
 class ClientServer(asyncio.Protocol):
     data_recieved = {}
 
+    def __init__(self):
+        self.transport = None
+
     def connection_made(self, transport):
         self.transport = transport
 
     def data_recieved(self, data):
-        resp = process_data(data.decode())
+        resp = ClientServer.save(data.decode())
         self.transport.write(resp.encode())
 
     @staticmethod
@@ -40,21 +42,24 @@ class ClientServer(asyncio.Protocol):
             raw = input.split(' ')
             if raw[0] == request.get:
                 if raw[1] == request.all:
-                    for key in data_recieved:
-                        for value in data_recieved[key]:
-                            output += str(key) + ' ' + str(value[0]) + ' ' + str(value[1])
-                    return 'ok\n' + output + '\n\n'
-                if raw[1] in data_recieved:
-                    for value in data_recieved[raw[1]]:
-                        output += str(raw[1]) + ' ' + str(value[0]) + ' ' + str(value[1])
+                    if ClientServer.data_recieved:
+                        for key in ClientServer.data_recieved:
+                            for value in ClientServer.data_recieved[key]:
+                                output += str(key) + ' ' + str(value[0]) + ' ' + str(value[1]) +  '\n'
+                        return 'ok\n' + output + '\n\n'
+                    else:
+                        return 'ok\n\n'
+                if raw[1] in ClientServer.data_recieved:
+                    for value in ClientServer.data_recieved[raw[1]]:
+                        output += str(raw[1]) + ' ' + str(value[0]) + ' ' + str(value[1]) + '\n'
                     return 'ok\n' + output + '\n\n'
                 else:
                     return 'ok\n\n'
             elif raw[0] == request.put:
                 try:
-                    if raw[1] not in data_recieved:
-                        data_recieved[raw[1]] = []
-                    bisect.insort(data[raw[1]], (raw[2], raw[3]))
+                    if raw[1] not in ClientServer.data_recieved:
+                        ClientServer.data_recieved[raw[1]] = []
+                    ClientServer.data_recieved[raw[1]].append((raw[2], raw[3]))
                     return 'ok\n\n'
                 except Exception:
                     return 'error\nwrong command\n\n'
@@ -79,4 +84,4 @@ class ClientServer(asyncio.Protocol):
 # loop.close()
 
 run_server('127.0.0.1', 8888)
-''' som '''
+# ''' som '''
