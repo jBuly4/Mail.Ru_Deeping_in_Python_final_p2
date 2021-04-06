7429250cdc500d7a51b9a081fb57e07a15061c32
jBuly4 <ibuly4@gmail.com>
2021-04-06 22:28:38 +0300

    fucking opechating!!! recEived!!

--- server.py
+++ server.py
@@ -4,10 +4,8 @@ from collections import namedtuple
 requests = namedtuple('requests', 'put get all') # all = '*\n'
 request = requests(put = 'put', get = 'get', all = '*\n')
 
-# print(request.put)
 
 def run_server(host, port):
-    # print(host, port)
     server_loop = asyncio.get_event_loop()
     server_obj = server_loop.create_server(ClientServer, host, port) #creates a TCP server and returns server object. Server objects are asynchronous context managers
 
@@ -21,13 +19,12 @@ def run_server(host, port):
     server_loop.run_until_complete(server.wait_closed())
     server_loop.close()
 
-    return
 
 class ClientServer(asyncio.Protocol):
     data_to_save = {}
 
-    def __init__(self):
-        self.transport = None
+    # def __init__(self):
+    #     self.transport = None
 
     def connection_made(self, transport):
         print('start connection')
@@ -35,10 +32,10 @@ class ClientServer(asyncio.Protocol):
         print('connection made')
         print(self.transport)
 
-    def data_recieved(self, data):
-        print('data recieved: {}'.format(data.decode()))
-        resp = self.save(data.decode())
-        self.transport.write(resp.encode())
+    def data_received(self, data):
+        print('data received: {}'.format(data))
+        # resp = self.save(data.decode())
+        # self.transport.write(resp.encode())
 
     # @staticmethod
     def save(self, input):
@@ -66,7 +63,7 @@ class ClientServer(asyncio.Protocol):
                 try:
                     if raw[1] not in self.data_to_save:
                         self.data_to_save[raw[1]] = []
-                    self.data_to_save[raw[1]].append((raw[2], raw[3]))
+                    data_to_save[raw[1]].append((raw[2], raw[3]))
                     return 'ok\n\n'
                 except Exception:
                     return 'error\nwrong command\n\n'
@@ -75,20 +72,4 @@ class ClientServer(asyncio.Protocol):
         except Exception:
             return 'error\nwrong command\n\n'
 
-
-#loop = asyncio.get_event_loop() #moved to run_server func
-#coro = loop.create_server(ClientServer, '127.0.0.1', 8181)
-
-# server = loop.run_until_complete(coro)
-#
-# try:
-#     loop.run_forever()
-# except KeyboardInterrupt:
-#     pass
-
-# server.close()
-# loop.run_until_complete(server.wait_closed())
-# loop.close()
-
 run_server('127.0.0.1', 8888)
-# ''' som '''
