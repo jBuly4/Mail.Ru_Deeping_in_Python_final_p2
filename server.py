714df72ba20d955bde61fbb90b082c9d524d7e0c
Study <ibuly4@gmail.com>
2021-04-07 20:48:20 +0300

    refactored

--- server.py
+++ server.py
@@ -19,57 +19,87 @@ def run_server(host, port):
     server_loop.run_until_complete(server.wait_closed())
     server_loop.close()
 
-
-class ClientServer(asyncio.Protocol):
+class Storage:
     data_to_save = {}
 
-    # def __init__(self):
-    #     self.transport = None
+    def return_all(self):
+        output = ''
+        if data_to_save:
+            for key in data_to_save:
+                for value in data_to_save[key]:
+                    output += f'{key} {value[0]} {value[1]}\n'
+                    # output += str(key) + ' ' + str(value[0]) + ' ' + str(value[1]) +  '\n'
+            return 'ok\n' + output + '\n'
+        else:
+            return 'ok\n\n'
+
+    def return_part(self, key):
+        output = ''
+        if key in data_to_save:
+            for value in data_to_save[key]:
+                output += f'{key} {value[0]} {value[1]}\n'
+                # output += str(key) + ' ' + str(value[0]) + ' ' + str(value[1]) + '\n'
+            return 'ok\n' + output + '\n'
+        else:
+            return 'ok\n\n'
+
+    def put_in(self, raw):
+        try:
+            key, value, timestamp = raw.split()
+            if key not in data_to_save:
+                data_to_save[key] = []
+            data_to_save[key].append((value, timestamp))
+            return 'ok\n\n'
+        except Exception:
+            return 'error\nwrong command\n\n'
+
+        return
+
+class ClientServer(asyncio.Protocol):
 
     def connection_made(self, transport):
-        print('start connection')
         self.transport = transport
-        print('connection made')
         print(self.transport)
 
     def data_received(self, data):
         print('data received: {}'.format(data))
-        # resp = self.save(data.decode())
-        # self.transport.write(resp.encode())
+        resp = self.save(data.decode())
+        self.transport.write(resp.encode())
 
     # @staticmethod
     def save(self, input):
         output = ''
-        print(input.decode())
+        print('save input {}'.format(input))
         try:
-            # print(input)
-            raw = input.split(' ')
-            if raw[0] == request.get:
-                if raw[1] == request.all:
-                    if self.data_to_save:
-                        for key in self.data_to_save:
-                            for value in self.data_to_save[key]:
-                                output += str(key) + ' ' + str(value[0]) + ' ' + str(value[1]) +  '\n'
-                        return 'ok\n' + output + '\n'
-                    else:
-                        return 'ok\n\n'
-                if raw[1] in self.data_to_save:
-                    for value in self.data_to_save[raw[1]]:
-                        output += str(raw[1]) + ' ' + str(value[0]) + ' ' + str(value[1]) + '\n'
-                    return 'ok\n' + output + '\n'
-                else:
-                    return 'ok\n\n'
-            elif raw[0] == request.put:
-                try:
-                    if raw[1] not in self.data_to_save:
-                        self.data_to_save[raw[1]] = []
-                    data_to_save[raw[1]].append((raw[2], raw[3]))
-                    return 'ok\n\n'
-                except Exception:
-                    return 'error\nwrong command\n\n'
+            query, input = input.split(' ', maxsplit=1)
+            if query == request.get:
+                return getting(input)
+
+            elif query == request.put:
+                return putting(input)
+
             else:
                 return 'error\nwrong command\n\n'
         except Exception:
             return 'error\nwrong command\n\n'
+
+    def getting(self, input):
+        try:
+            key = input.strip('\n')
+
+            if key == request.all:
+                return Storage.return_all()
+            else:
+                return Storage.return_part(key)
+
+        except Exception:
+            return 'error\nwrong command\n\n'
+
+    def putting(self, input):
+
+        raw = input.strip('\n')
+
+        return Storage.put_in(raw)
+
 
 run_server('127.0.0.1', 8888)
